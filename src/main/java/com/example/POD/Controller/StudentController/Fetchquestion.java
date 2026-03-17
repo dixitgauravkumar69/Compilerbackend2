2c51ba80e43a35357dd1195ac6c97c7290da49c0
Gaurav Kumar Dixit <dixitgauravkumar69@gmail.com>
2026-03-17 16:15:18 +0530

    Added students can see by tecacher according to problem statements

--- src/main/java/com/example/POD/Controller/StudentController/Fetchquestion.java
+++ src/main/java/com/example/POD/Controller/StudentController/Fetchquestion.java
@@ -3,10 +3,11 @@ package com.example.POD.Controller.StudentController;
 import com.example.POD.Entity.ProblemStatement;
 import com.example.POD.Repository.ProblemStatementRepo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,12 +19,13 @@ public class Fetchquestion {
 
     private final ProblemStatementRepo problemStatementRepo;
     @GetMapping("/getQuestions")
-    public List<ProblemStatement> getQuestions()
+    public Page<ProblemStatement> getQuestions(@RequestParam(defaultValue = "0") int page,
+                                               @RequestParam(defaultValue = "5") int size)
     {
-       List<ProblemStatement> mess= problemStatementRepo.findByAssignedTrue();
+        // PageRequest object create karein (Page index 0 se start hota hai)
+        Pageable pageable = PageRequest.of(page, size);
+       return problemStatementRepo.findByAssignedTrue(pageable);
 
-
-       return mess;
     }
 }
 
--- src/main/java/com/example/POD/Repository/ProblemStatementRepo.java
+++ src/main/java/com/example/POD/Repository/ProblemStatementRepo.java
@@ -1,14 +1,16 @@
 package com.example.POD.Repository;
 
 import com.example.POD.Entity.ProblemStatement;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface ProblemStatementRepo extends JpaRepository <ProblemStatement,Long>{
 
 
-    List<ProblemStatement> findByAssignedTrue();
+    Page<ProblemStatement> findByAssignedTrue(Pageable pagable);
 
 }
