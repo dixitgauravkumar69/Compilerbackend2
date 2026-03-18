000f1e730c8dcd2d5990d6b66e8b2e7a0845a50d
Gaurav Kumar Dixit <dixitgauravkumar69@gmail.com>
2026-03-18 14:09:50 +0530

    Port of SMTP change 562-> 465

--- src/main/java/com/example/POD/PodApplication.java
+++ src/main/java/com/example/POD/PodApplication.java
@@ -2,8 +2,11 @@ package com.example.POD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+
+@EnableAsync
 public class PodApplication {
 
 	public static void main(String[] args) {
